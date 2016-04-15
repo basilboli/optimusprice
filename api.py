b59400b1b0df8d0b2d7bf3d74341be14d9f8417c
basilboli <basilboli@gmail.com>
2016-04-15 15:30:21 +0200

    update api.py

--- api.py
+++ api.py
@@ -23,7 +23,7 @@ products = [
         "id": 2,
         "brand": "ASUS",
         "name": "ASUS GeForce GTX 950 GTX950-OC-2GD5",
-        "price": "180",
+        "price": "168",
         "image": "http://media.ldlc.com/ld/products/00/03/58/29/LD0003582993_2.jpg"
     },
     {
